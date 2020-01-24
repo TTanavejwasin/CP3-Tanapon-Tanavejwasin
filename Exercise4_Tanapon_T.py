177a08e6ada70388629f580f48db356f896caf84
TTanavejwasin <60262100+TTanavejwasin@users.noreply.github.com>
2020-01-24 21:48:42 +0700

    Create Exercise4_Tanapon_T.py

---
+++ Exercise4_Tanapon_T.py
@@ -0,0 +1,11 @@
+
+FoundEng = 70
+GenBus = 80
+IntroCom = 90.5
+ComPro = 100
+
+print("---Your Score---")
+print("Foundation English :","=",FoundEng)
+print("General Business :","=",GenBus)
+print("Introduction to Computer Systems :","=",IntroCom)
+print("Computer Programming :","=",ComPro)
