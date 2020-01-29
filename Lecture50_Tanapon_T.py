60d67469186a94d5c4209cbb0c1a8b5328d94b8a
TTanavejwasin <60262100+TTanavejwasin@users.noreply.github.com>
2020-01-29 16:24:20 +0700

    Create Lecture50_Tanapon_T.py

---
+++ Lecture50_Tanapon_T.py
@@ -0,0 +1,17 @@
+def addNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multipleNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+
+addNumber(4,5)
+minusNumber(4,5)
+multipleNumber(4,5)
+divideNumber(4,5)
+
+
+
+
