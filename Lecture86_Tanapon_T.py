1cd5bac7d2370a5e5f37475e3dab4899163b4247
TTanavejwasin <60262100+TTanavejwasin@users.noreply.github.com>
2020-02-03 19:02:51 +0700

    Lecture86

---
+++ Lecture86_Tanapon_T.py
@@ -0,0 +1,16 @@
+import csv
+
+def readCSV():
+    with open('File.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print(f'{"".join(row)}')
+                line_count += 1
+            else:
+                print(f'{row[0]}{row[1]}{row[2]}')
+                line_count += 1
+        print(f'Processed {line_count} lines.')
+
+readCSV()
